c48c97eaf39c7c21519d23896e28b3e863d4ff5b
creezed <glamber3@mail.ru>
2023-01-01 19:17:47 +0300

    cross env & typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import entities from '../utils/typeorm';
+
+export const getTypeormConfig = async (): Promise<TypeOrmModuleOptions> => ({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_NAME,
+  synchronize: true,
+  entities: entities,
+});
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
